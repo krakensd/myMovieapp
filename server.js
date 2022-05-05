a8c8d14a9e9b97910a7c58863ffa4c0b20c1f14b
Kraken <bob.lembke@hotmail.com>
2022-05-05 11:32:38 -0700

    #4

--- server.js
+++ server.js
@@ -1,21 +1,69 @@
- // Require modules
- const express = require('express');
- 	
- // Create the Express app
- const app = express();
- 	
- // Configure the app (app.set)
- 	
- 	
- // Mount middleware (app.use)
- 	
- 	
- // Mount routes
- app.get('/', function (req, res) {
-	  res.send('<h1>Bobs Universe!</h1>');
- });
- 	
- // Tell the app to listen on port 3000
- app.listen(3000, function() {
-   console.log('Listening on Bobs port 3000');
- });
+// Require modules
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var session = require('express-session');
+var passport = require('passport');
+require('dotenv').config();
+require('./config/database');
+require('./config/passport');
+var methodOverride = require('method-override');
+
+var indexRouter = require('./routes/index');
+var moviesRouter = require('./routes/movies');
+var reviewsRouter = require('./routes/reviews');
+var performersRouter = require('./routes/performers');
+// Configure the app
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
+
+app.use(methodOverride('_method'));
+
+// OA code
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(function (req, res, next) {
+  res.locals.user = req.user;
+  next();
+});
+
+app.use('/', indexRouter);
+app.use('/movies', moviesRouter);
+app.use('/', reviewsRouter);
+app.use('/', performersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
